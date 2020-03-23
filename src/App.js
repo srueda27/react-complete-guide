aa2e760e828eeca4e2a7345875fcab9d7da19224
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-03-22 20:28:17 -0500

    add manipulating state with clickHandler

--- src/App.js
+++ src/App.js
@@ -7,14 +7,24 @@ class App extends Component {
     persons: [
       { name: 'Santi', age: '28' },
       { name: 'Checho', age: '29' }
-    ]
+    ],
+    anotherState: "Another State"
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Santiago', age: '28' },
+        { name: 'Checho', age: '29' }
+      ]
+    });
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Eating</Person>
       </div>
